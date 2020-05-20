97e3894114b0ccfe4a49564e60c2638fb14d82a0
charlyalizadeh <alizadehcharly@gmail.com>
2020-05-20 19:57:34 +0200

    Tried to change the PCA display

--- knn.py
+++ knn.py
@@ -4,23 +4,28 @@ from sklearn.decomposition import PCA
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
-
+import matplotlib.cm as cm
 
 
 class KNN:
-    def __init__(self,nb_column = None,columns = None, index_label = -1):
+    def __init__(self,nb_column = None,columns = None):
         assert(not (nb_column == None and columns == None)),'You need to specify either nb_column or columns'
-        # By default the column names will C0, C1, C2...
+        # By default the column names will c0, c1, c2...
         if columns == None:
-            columns = ['C' + str(i) for i in range(nb_column - 1)]
-            columns.append('Label')
+            columns = ['c' + str(i) for i in range(nb_column - 1)]
+            columns.append('label')
             columns.append('Distance')
         self.df = pd.DataFrame(columns = columns)
 
-        # By default we consider the label in the last column
-        self.index_label = len(self.df.columns) - 1 if index_label == -1 else index_label
 
+    def get_mat_conf(self,filenamme = None,df_training = None):
+        assert(not (filename == None and df_training == None)),'You need so specify either filename or df_training'
+        if df_training == None:
+            df_training = pd.read_csv(filename,names = self.df.columns)
+        
+        
+       
+        
     def load_data(self, filename, sep = ','):
         if self.is_valid_file(filename,sep = sep):
             temp_df = pd.read_csv(filename,names = self.df.columns, sep = sep)
@@ -40,7 +45,7 @@ class KNN:
         self.df.sort_values(by = ['Distance'])
         
         # Get the label with highest frequence between the first k columns of self.df
-        best_label = self.df.head(k)['Label'].mode().iloc[0]
+        best_label = self.df.head(k)['label'].mode().iloc[0]
         return best_label
 
     def get_eucl_dist(self,p1,p2):
@@ -59,6 +64,10 @@ class KNN:
                     return False 
         return True
 
+    def is_valid_df(self,df,has_label = True):
+
+
+
     def display_df(self):
         print(self.df)
         print(self.df.describe(),end="\n\n")
@@ -74,16 +83,16 @@ class KNN:
         principalComponents = pca.fit_transform(x)
         principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
 
-        finalDf = pd.concat([principalDf, self.df[['Label']]], axis = 1)
+        finalDf = pd.concat([principalDf, self.df[['label']]], axis = 1)
 
         fig = plt.figure(figsize = (8,8))
         ax = fig.add_subplot(1,1,1) 
         ax.set_xlabel('Principal Component 1', fontsize = 15)
         ax.set_ylabel('Principal Component 2', fontsize = 15)
         ax.set_title('2 component PCA', fontsize = 20)
-        labels = self.df.Label.unique()
+        labels = self.df.label.unique()
         for label,i in zip(labels,range(len(self.df.index))):
-            indicesToKeep = finalDf['Label'] == label
+            indicesToKeep = finalDf['label'] == label
             ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
                        , finalDf.loc[indicesToKeep, 'principal component 2']
                        , c = 'C' + str(i)
@@ -91,7 +100,10 @@ class KNN:
             i+=1
         ax.legend(labels)
         ax.grid()
+        print(pca.explained_variance_ratio_)                
         plt.show()
+
+            
 
 test = KNN(5)
 test.load_data("data.csv",sep = ';')
